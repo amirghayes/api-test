d8323fcb3183509935c23b166e315b9690a7fae2
Amir Ghayes <amirghayes@gmail.com>
2023-02-28 23:23:08 +0200

    First commit

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  try {
+    const db = require('mysql2-promise')();
+    db.configure({
+      "host": "localhost",
+      "user": process.env.DB_USER,
+      "password": process.env.DB_PASS,
+      "database": process.env.DB_NAME
+    });
+
+    const query = await db.query('P1');
+    console.log(query);
+    res.status(200).json(query)
+
+  } catch (error) {
+    res.status(200).json(error)
+  }
+
 }
