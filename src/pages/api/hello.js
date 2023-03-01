ab94af32c6c6d185b95452da133e1b8259f5e4fc
Amir Ghayes <amirghayes@gmail.com>
2023-03-01 03:42:02 +0200

    Finally parsed

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,21 +1,36 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async function handler(req, res) {
+  const mysql = require('mysql2/promise');
+  let connection;
+  let result;
   try {
-    const db = require('mysql2-promise')();
-    db.configure({
-      "host": "localhost",
-      "user": process.env.DB_USER,
-      "password": process.env.DB_PASS,
-      "database": process.env.DB_NAME
+    connection = await mysql.createConnection({
+      host: '130.61.132.4',
+      port: 3306,
+      user: process.env.NEXT_PUBLIC_DB_USER,
+      password: process.env.NEXT_PUBLIC_DB_PASS,
+      database: process.env.NEXT_PUBLIC_DB_NAME
     });
 
-    const query = await db.query('CALL P1');
-    console.log(query.rows[0]);
-    res.status(200).json(query)
+    const [rows] = await connection.execute('CALL P1');
+    const rowData = rows[0][0];
+    result = `${rowData.ServerStatus},${rowData.ServerVersion}`;
+    // for (const row of rows) {
+    //   // Do something with each row
+    //   console.log(row[0]);
+    //   result += row[0].ID;
+    // }
+    res.status(200).send(result);
 
   } catch (error) {
-    res.status(200).json(error)
-  }
+    // Handle error
+    console.log(error);
+    res.status(200).json(error, process.env.NEXT_PUBLIC_DB_USER);
 
+  } finally {
+    if (connection) {
+      await connection.end();
+    }
+  }
 }
